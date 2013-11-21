fdbeba5c66a1263605501daf16d41b6b18c956f3
Josh Barber <lego99@gmail.com>
2013-11-21 07:34:58 -0500

    configuring grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+/*global module:false*/
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    coffee:{
+      options:{
+        bare: true
+      },
+      glob_to_multiple:{
+        expand: true,
+        cwd: 'app/',
+        src:['**/**/*.coffee'],
+        dest: 'app',
+        ext: ".js"
+      }
+    },
+
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {}
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['app/problems/**/*.js', 'app/tests/**/*.js']
+      }
+    },
+    qunit: {
+      files: ['app/tests/**/*.html']
+    },
+
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib_test: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test', 'qunit']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+
+  // Default task.
+  grunt.registerTask('default', ['coffee', 'jshint', 'qunit']);
+
+};
---
+++ README.md
@@ -0,0 +1,5 @@
+##Project Euler Solutions
+
+Every week (or close to that), I plan on taking on a new math challenge from Project Euler and solving it using Javascript and testing best practices. 
+
+You can view my solutions broken down by problem here, and writeups describing my thoughts at http://www.jorshasaur.us
