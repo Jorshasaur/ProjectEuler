82611887ccd15554252d4984bffd8c34d8007227
Josh Barber <lego99@gmail.com>
2013-12-11 07:30:46 -0500

    DRY tests

--- app/tests/problem1/tests.coffee
+++ app/tests/problem1/tests.coffee
@@ -2,21 +2,19 @@ assert = require "assert"
 Solution = require "../../problems/problem1/solution.js"
 
 describe "Solution", ->
+	beforeEach ->
+		@solution = new Solution()
   
 	it "should have factors of 3,5,6,9 below 10.", ->
-		solution = new Solution()
-		assert.deepEqual solution.findMultiples([3,5], 1, 10).result, [3,5,6,9]
+		assert.deepEqual @solution.findMultiples([3,5], 1, 10).result, [3,5,6,9]
 
 	it "should not repeat factors.", ->
-		solution = new Solution()
-		assert.deepEqual solution.findMultiples([3,5], 1, 18).result, [3,5,6,9,10,12,15]
+		assert.deepEqual @solution.findMultiples([3,5], 1, 18).result, [3,5,6,9,10,12,15]
 
 	it "should calculate the sum of 3,5,6,9 to be 23.", ->
-		solution = new Solution()
-		solution.findMultiples([3,5], 1, 10)
-		assert.equal solution.sum(), 23
+		@solution.findMultiples([3,5], 1, 10)
+		assert.equal @solution.sum(), 23
 
 	it "should calculate multiples to 999 with a sum of 233168.", ->
-		solution = new Solution()
-		solution.findMultiples([3,5], 1, 1000)
-		assert.equal solution.sum(), 233168
+		@solution.findMultiples([3,5], 1, 1000)
+		assert.equal @solution.sum(), 233168
--- app/tests/problem1/tests.js
+++ app/tests/problem1/tests.js
@@ -5,26 +5,21 @@ assert = require("assert");
 Solution = require("../../problems/problem1/solution.js");
 
 describe("Solution", function() {
+  beforeEach(function() {
+    return this.solution = new Solution();
+  });
   it("should have factors of 3,5,6,9 below 10.", function() {
-    var solution;
-    solution = new Solution();
-    return assert.deepEqual(solution.findMultiples([3, 5], 1, 10).result, [3, 5, 6, 9]);
+    return assert.deepEqual(this.solution.findMultiples([3, 5], 1, 10).result, [3, 5, 6, 9]);
   });
   it("should not repeat factors.", function() {
-    var solution;
-    solution = new Solution();
-    return assert.deepEqual(solution.findMultiples([3, 5], 1, 18).result, [3, 5, 6, 9, 10, 12, 15]);
+    return assert.deepEqual(this.solution.findMultiples([3, 5], 1, 18).result, [3, 5, 6, 9, 10, 12, 15]);
   });
   it("should calculate the sum of 3,5,6,9 to be 23.", function() {
-    var solution;
-    solution = new Solution();
-    solution.findMultiples([3, 5], 1, 10);
-    return assert.equal(solution.sum(), 23);
+    this.solution.findMultiples([3, 5], 1, 10);
+    return assert.equal(this.solution.sum(), 23);
   });
   return it("should calculate multiples to 999 with a sum of 233168.", function() {
-    var solution;
-    solution = new Solution();
-    solution.findMultiples([3, 5], 1, 1000);
-    return assert.equal(solution.sum(), 233168);
+    this.solution.findMultiples([3, 5], 1, 1000);
+    return assert.equal(this.solution.sum(), 233168);
   });
 });
